393ed457f2739e493417034aa457c50eb843950a
stewartad <austendean@gmail.com>
2021-09-19 16:11:51 -0700

    2.0 beta 1

--- seeker.py
+++ seeker.py
@@ -1,5 +1,7 @@
 from datetime import date, timezone
+import enum
 import os
+import re
 
 from discord.ext.commands.errors import BadArgument, UserNotFound
 from database import *
@@ -21,19 +23,62 @@ BASE_URL = API_HOST + '/seekerbot/api'
 NL = '\n'
 
 
+class NoGamesRecordedError(Exception):
+    def __init__(self, user, *args: object) -> None:
+        self.user = user
+        self.message = f'No games on record for {self.user} in this guild.'
+        super().__init__(*args)
+
+    def __str__(self) -> str:
+        return f'No games on record for {self.user} in this guild.'
+
+
 class MatchReportConverter(commands.Converter):
 
     async def convert(self, ctx: commands.Context, argument: str):
         # split string by spaces
-        args = argument.split(' ')
+        cmd_params = argument.split(' ')
 
         # <user1> X <user2> Y <description>
-        # <user1> <deck> X <user2> <deck> Y <description>
-        # <user1> X-Y <user2> <description>
-        # <user1> <deck> X-Y <user2> <deck> <description>
+        # <user1> X <deck> <user2> Y <deck>
 
-        # Comnmands are always user deck score
+        # Comnmands are always user score deck
 
+        # scan args for members, associate non-member strings with the most recently parsed member
+        member_converter = commands.MemberConverter()
+        cmd_params_len = len(cmd_params)
+        sorted_args = {}
+
+        user_pattern = re.compile('<@!\d+>')
+
+        # get_users
+        for i in range(cmd_params_len):
+            param = cmd_params[i]
+            if user_pattern.match(param):
+                sorted_args[i] = {
+                        'user': param,
+                        'games': int(cmd_params[i+1])
+                }
+
+                deck_strs = []
+                deck_first = i + 2
+                deck_last = deck_first
+                while deck_last >= deck_first and deck_last < cmd_params_len:
+                    if not user_pattern.match(cmd_params[deck_last]):
+                        deck_strs.append(cmd_params[deck_last])
+                        deck_last += 1
+                    else:
+                        break
+                    
+
+                if deck_last > deck_first:
+                    sorted_args[i]['deck'] = " ".join(cmd_params[deck_first:deck_last])
+
+        # if members are less than 2, this is an invalid command
+        if len(sorted_args.keys()) < 2:
+            raise BadArgument("Not enough users in game")
+
+        # construct match report
         match = {
             'reports': [],
             'channel_id': ctx.channel.id,
@@ -43,54 +88,21 @@ class MatchReportConverter(commands.Converter):
             },
         }
 
-        # scan args for members, associate non-member strings with the most recently parsed member
-        member_converter = commands.MemberConverter()
-        last_user = None
-        sorted_args = {}
-        for arg in args:
+        for data in sorted_args.values():
+            user = data.get('user')
             try:
-                last_user = await member_converter.convert(ctx, arg)
-                sorted_args[last_user] = []
-            except BadArgument:
-                if last_user is None:
-                    raise BadArgument('Insufficient number of users')
-                else:
-                    sorted_args[last_user].append(arg)
-
-        # if members are less than 2, this is an invalid command
-        if len(sorted_args.keys()) < 2:
-            raise BadArgument()
-
-        for user, data in sorted_args.items():
-            report = {
+                converted_user = await member_converter.convert(ctx, user)
+                report = {
                 'user': {
-                    'user_id': user.id,
-                    'name': f'{user.name}#{user.discriminator}'
+                    'user_id': converted_user.id,
+                    'name': f'{converted_user.name}#{converted_user.discriminator}'
                 },
-                'games': '',
-                'deck': '',
-                # 'description': '',
+                'games': data.get('games'),
+                'deck': data.get('deck')
             }
-
-            if len(sorted_args.keys()) == 2:
-                # scores = args[1].split('-')
-                pass
-
-            # find score, deck, and description
-            for i, arg in enumerate(data):
-                try:
-                    report['games'] = int(arg)
-                except ValueError:
-                    if report['deck'] == '':
-                        report['deck'] = arg
-                    # elif user == last_user and i < len(data) + 1:
-                    #     report['description'] = data[i + 1]
-                    else:
-                        raise BadArgument()
-
-            if report['games'] == '':
-                raise BadArgument()
-
+            except BadArgument:
+                raise BadArgument(f'Invalid user: {user}')
+            
             match['reports'].append(report)
 
         # print(match)
@@ -122,7 +134,7 @@ class SeekerCog(commands.Cog):
 
     @undo.error
     async def undo_error(self, ctx, error):
-        await ctx.send(f'Error: {str(error)}')
+        await ctx.send(str(error))
 
     @commands.command(name='report')
     async def report(self, ctx, *, report: MatchReportConverter):
@@ -154,9 +166,7 @@ class SeekerCog(commands.Cog):
             stats_req = requests.get(f'{BASE_URL}/leaderboard/{user.id}', params={'guild': guild_id, 'date': int(time)}, auth=(ADMIN, PASSWD))
 
             if not stats_req.ok:
-                f = open('log.html', 'w')
-                f.write(stats_req.text)
-                return f'User {user} not found'
+                raise NoGamesRecordedError(user)
 
             stats = stats_req.json()
             entries.append(entry_fmt.format(self.timeframes[i], stats.get('games_played'), stats.get('games_won'), stats.get('winrate')))
@@ -169,7 +179,7 @@ class SeekerCog(commands.Cog):
     @stats.error
     async def stats_error(self, ctx, error):
         logging.exception('')
-        await ctx.send(f'Error: {str(error)}')
+        await ctx.send(f'Error: {error.original}')
 
     @commands.command(name='leaderboard')
     async def leaderboard(self, ctx, count: typing.Optional[int] = 10, time: typing.Optional[str] = 'week'):
@@ -201,6 +211,37 @@ class SeekerCog(commands.Cog):
     async def leaderboard_error(self, ctx, error):
         logging.exception('')
         await ctx.send(f'Error: {str(error)}')
+
+    @commands.command(name='decks')
+    async def decks(self, ctx, count: typing.Optional[int] = 10, time: typing.Optional[str] = 'week'):
+        '''
+        !leaderboard [count] [time]
+        '''
+        if time.upper() not in self.timeframes:
+                await ctx.send('Invalid time. Use week, month, year, or all')
+                return
+
+        guild_id = ctx.guild.id
+        # guild_id = 315538837474508800
+        leaderboard_req = requests.get(f'{BASE_URL}/decks', 
+            params={
+                'guild': ctx.guild.id,
+                'channel_id': ctx.channel.id
+                }, 
+            auth=(ADMIN, PASSWD))
+        leaderboard = leaderboard_req.json()
+
+        if leaderboard_req.status_code != 200:
+            raise requests.RequestException(f'Error: response {leaderboard_req.status_code}')
+
+        print(leaderboard)
+
+        entry_fmt = '{:2}. {:<16} {:<6} {:<6} {:.2%}'
+        header_fmt = '{:2}. {:<16} {:<6} {:<6} {:4}'
+        entries = [entry_fmt.format(i + 1, entry.get('deck')[:16], entry.get('games_played'), entry.get('games_won'), entry.get('winrate')) for i, entry in enumerate(leaderboard[:count])]
+        
+        message = f'''```{header_fmt.format('No', 'User', 'Games', 'Won', 'Win %')}{NL}{NL.join(entries)}```'''
+        await ctx.send(message)
 
 
 TOKEN = os.getenv('DISCORD_TOKEN')
